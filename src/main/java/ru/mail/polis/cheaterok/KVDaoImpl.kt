e48b8854fb9c865b1bf2246302ab29e475c417d0
cheaterok <1cheaterok1@gmail.com>
2019-01-16 18:42:28 +0300

    Оставляет "надгробия" вместо безвозвратного удаления

--- src/main/java/ru/mail/polis/cheaterok/KVDaoImpl.kt
+++ src/main/java/ru/mail/polis/cheaterok/KVDaoImpl.kt
@@ -4,8 +4,14 @@ import java.io.File
 
 import org.rocksdb.RocksDB
 import org.rocksdb.Options
+import org.rocksdb.WriteOptions
+import org.rocksdb.WriteBatch
 
 import ru.mail.polis.KVDao
+
+
+fun aliveKey(key: ByteArray): ByteArray = byteArrayOf(0) + key
+fun deadKey(key: ByteArray): ByteArray = byteArrayOf(1) + key
 
 
 class KVDaoImpl(val data: File) : KVDao {
@@ -13,18 +19,26 @@ class KVDaoImpl(val data: File) : KVDao {
     val options = Options().setCreateIfMissing(true)
     val database = RocksDB.open(options, data.path)
 
+    val writeOptions = WriteOptions()
 
     @Throws(NoSuchElementException::class)
     override fun get(key: ByteArray): ByteArray {
-        return database.get(key) ?: throw NoSuchElementException("No value for key '$key'")
+        return database.get(aliveKey(key)) ?: throw NoSuchElementException("No value for key '$key'")
     }
 
     override fun upsert(key: ByteArray, value: ByteArray) {
-        database.put(key, value)
+        database.put(aliveKey(key), value)
     }
 
     override fun remove(key: ByteArray) {
-        database.singleDelete(key)
+        val data = database.get(aliveKey(key)) ?: return
+
+        val writeBatch = WriteBatch()
+
+        writeBatch.remove(aliveKey(key))
+        writeBatch.put(deadKey(key), data)
+
+        database.write(writeOptions, writeBatch)
     }
 
     override fun close() {
