6a81b790ee376ab8bb6d1db43f5c567dd58bb69e
cheaterok <1cheaterok1@gmail.com>
2019-01-17 20:44:03 +0300

    Добавляет поддержку "надгробий" и меток времени при сохранении данных

--- src/main/java/ru/mail/polis/cheaterok/KVDaoImpl.kt
+++ src/main/java/ru/mail/polis/cheaterok/KVDaoImpl.kt
@@ -4,14 +4,8 @@ import java.io.File
 
 import org.rocksdb.RocksDB
 import org.rocksdb.Options
-import org.rocksdb.WriteOptions
-import org.rocksdb.WriteBatch
 
 import ru.mail.polis.KVDao
-
-
-fun aliveKey(key: ByteArray): ByteArray = byteArrayOf(0) + key
-fun deadKey(key: ByteArray): ByteArray = byteArrayOf(1) + key
 
 
 class KVDaoImpl(val data: File) : KVDao {
@@ -19,26 +13,17 @@ class KVDaoImpl(val data: File) : KVDao {
     val options = Options().setCreateIfMissing(true)
     val database = RocksDB.open(options, data.path)
 
-    val writeOptions = WriteOptions()
-
     @Throws(NoSuchElementException::class)
     override fun get(key: ByteArray): ByteArray {
-        return database.get(aliveKey(key)) ?: throw NoSuchElementException("No value for key '$key'")
+        return database.get(key) ?: throw NoSuchElementException("No value for key '$key'")
     }
 
     override fun upsert(key: ByteArray, value: ByteArray) {
-        database.put(aliveKey(key), value)
+        database.put(key, value)
     }
 
     override fun remove(key: ByteArray) {
-        val data = database.get(aliveKey(key)) ?: return
-
-        val writeBatch = WriteBatch()
-
-        writeBatch.remove(aliveKey(key))
-        writeBatch.put(deadKey(key), data)
-
-        database.write(writeOptions, writeBatch)
+        database.singleDelete(key)
     }
 
     override fun close() {
--- src/main/java/ru/mail/polis/cheaterok/KVServiceImpl.kt
+++ src/main/java/ru/mail/polis/cheaterok/KVServiceImpl.kt
@@ -9,7 +9,23 @@ import ru.mail.polis.KVService
 import ru.mail.polis.KVDao
 
 
-fun quorum(nodesCount: Int): Int = if ((nodesCount % 2) == 0) {nodesCount / 2} else {nodesCount / 2 + 1}
+fun quorum(nodesCount: Int): Int = nodesCount / 2 + 1
+
+fun wrapData(data: ByteArray, isAlive: Boolean = true): ByteArray {
+    val timestamp = System.currentTimeMillis().toBigInteger().toByteArray();
+    val aliveBit = if (isAlive) byteArrayOf(1) else byteArrayOf(0)
+
+    return aliveBit + timestamp + data
+}
+
+fun unwrapData(data: ByteArray): Triple<Boolean, Long, ByteArray> {
+    val aliveBit = data[0].toInt() != 0
+    // В душе не знаю почему, но Long.toBigInteger().toByteArray() в результате имеет такой размер
+    val timestamp = BigInteger(data.sliceArray(1..6)).toLong()
+    val payload = data.sliceArray(7..data.size - 1)
+
+    return Triple(aliveBit, timestamp, payload)
+}
 
 
 class KVServiceImpl(val port: Int, val dao: KVDao, val topology: Set<String>) : KVService {
@@ -53,25 +69,37 @@ class KVServiceImpl(val port: Int, val dao: KVDao, val topology: Set<String>) : 
             val key = try {queryParams("id").toByteArray()} catch (e: IllegalStateException) {null}
             val replicas = try {queryParams("replicas")} catch (e: IllegalStateException) {null}
 
+            // Неправильные запросы
             if (key == null) {
                 status(400)
                 "Empty request"
             } else if (key.isEmpty()) {
                 status(400)
                 "No key specified"
-            } else {
+            } 
+            // Мякотка начинается здесь
+            else {
+                val wrappedData = try {dao.get(key)} catch (e: NoSuchElementException) {null}
+                // Старое API
                 if (replicas == null) {
-                    val data = try {dao.get(key)} catch (e: NoSuchElementException) {null}
-                    if (data == null) {
+                    if (wrappedData == null) {
                         status(404)
                         "Value not found"
                     } else {
-                        status(200)
-                        data
+                        val (isAlive, _, data) = unwrapData(wrappedData)
+                        if (isAlive) {
+                            status(200)
+                            data
+                        }
+                        else {
+                            status(404)
+                            "Value is deleted"
+                        }
                     }
-                } else {
+                } 
+                // Новое API
+                else {
                     val nodesToAsk = getNodesToAsk(key, replicas)
-                    
                     "TODO"
                 }
             }
@@ -83,43 +111,42 @@ class KVServiceImpl(val port: Int, val dao: KVDao, val topology: Set<String>) : 
         }
 
         server.put("/v0/entity") {
-            try { 
-                val key = queryParams("id").toByteArray()
+            val key = try {queryParams("id").toByteArray()} catch (e: IllegalStateException) {null}
 
-                if (key.isEmpty()) {
-                    status(400)
-                    "No key specified"
-                } else {
-                    dao.upsert(key, request.bodyAsBytes())
-
-                    status(201)
-                    "Key added"
-                }
-            }
-            catch (e: IllegalStateException) {
+            if (key == null) {
                 status(400)
                 "Empty request"
-            }
+            } else if (key.isEmpty()) {
+                status(400)
+                "No key specified"
+            } else {
+                val data = request.bodyAsBytes()
+                dao.upsert(key, wrapData(data))
 
+                status(201)
+                "Key added"
+            }
         }
 
         server.delete("/v0/entity") {
-            try { 
-                val key = queryParams("id").toByteArray()
+            val key = try {queryParams("id").toByteArray()} catch (e: IllegalStateException) {null}
 
-                if (key.isEmpty()) {
-                    status(400)
-                    "Key is empty"
-                } else {
-                    dao.remove(key)
-
-                    status(202)
-                    "Key removed"
-                }
-            }
-            catch (e: IllegalStateException) {
+            if (key == null) {
                 status(400)
                 "Empty request"
+            } else if (key.isEmpty()) {
+                status(400)
+                "Key is empty"
+            } else {
+                val wrappedData = try {dao.get(key)} catch (e: NoSuchElementException) {null}
+
+                if (wrappedData != null) {
+                    val (_, _, data) = unwrapData(wrappedData)
+                    dao.upsert(key, wrapData(data, isAlive=false))
+                }
+
+                status(202)
+                "Key removed"
             }
         }
     }
